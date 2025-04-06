d9a4de4681cdb9901b451c2c9ee7bdc8d010bf7e
Milan Zeki <mzeki1@sheffield.ac.uk>
2025-04-06 17:22:44 +0100

    Create bark action file

---
+++ scripts/bark.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image,CompressedImage,Range,Imu
+from geometry_msgs.msg import Twist,Pose
+
+import miro_msgs
+from miro_msgs.msg import platform_config,platform_sensors,platform_state,platform_mics,platform_control,core_state,core_control,core_config,bridge_config,bridge_stream
+
+import opencv_apps
+from opencv_apps.msg import CircleArrayStamped
+
+import math
+import numpy
+import time
+import sys
+from miro_constants import miro
+
+from datetime import datetime
+
+"""
+bark.py implements the action corresponding to the "bark" command
+MiRo responds by playing a bark audio file
+"""
+
+class Bark():
+
+    def __init__(self):
+        ## Node rate
+        self.rate = rospy.get_param('rate',200)
