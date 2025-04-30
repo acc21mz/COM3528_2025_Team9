0386e6b387246c819106156e63bd07fc5eecfb92
Milan Zeki <mzeki1@sheffield.ac.uk>
2025-04-30 11:32:01 +0100

    Add MiRo play dead code

--- scripts/play_dead.py
+++ scripts/play_dead.py
@@ -22,11 +22,40 @@ from datetime import datetime
 
 """
 play_dead.py implements the action corresponding to the "play dead" command
-MiRo responds by stopping all motion, and dropping its head. (idea; also close eyes?)
+MiRo responds by stopping all motion, dropping its head, closing its eyes, and drooping its tail and ears
 """
 
 class PlayDead():
 
     def __init__(self):
+
         ## Node rate
         self.rate = rospy.get_param('rate',200)
+
+        self.pub_platform_control = rospy.Publisher('/miro_sleep',platform_control,queue_size=0)
+    
+    ## Function that sets the parameters of the structure platform_control corresponding to action "Sleep"
+    def miro_dead(self):
+
+        r = rospy.Rate(self.rate)
+        q = platform_control()
+        
+        while True:
+            try:
+                q.eyelid_closure = 1.0  # close eyelids
+                q.body_config = [0.0,1.0,0.2,0.1]  # move head down
+                q.body_config_speed = [0.0,-1.0,-1.0,-1.0]    # control speed at which head falls
+                q.tail = -1.0 # bring tail down
+                q.ear_rotate = [1.0,1.0]  # point ears down
+                self.pub_platform_control.publish(q)
+            except KeyboardInterrupt:
+                self.pub_platform_control.publish(q)                
+                break
+            r.sleep()
+
+       
+
+if __name__== '__main__':
+    rospy.init_node('sleep', disable_signals=True)
+    sleep = PlayDead()
+    sleep.miro_dead()
